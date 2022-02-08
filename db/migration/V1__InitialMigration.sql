75e16730d96c75dedb709717d1f56036bff779d0
Farye Nwede <farye@aeekay.com>
2022-02-07 22:43:50 -0800

    chore(db): Fixing the timestamp type to timestamptz

--- db/migration/V1__InitialMigration.sql
+++ db/migration/V1__InitialMigration.sql
@@ -5,13 +5,13 @@ CREATE TABLE builds (
     repository varchar(100) NOT NULL,
     git_commit varchar(72) NOT NULL, 
     branch varchar(256),
-    created timestampz DEFAULT NOW(),
-    updated timestampz DEFAULT NOW()
+    created timestamptz DEFAULT NOW(),
+    updated timestamptz DEFAULT NOW()
 );
 
 CREATE TABLE resources (
     id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     name varchar(256) NOT NULL,
-    created timestampz DEFAULT NOW(),
-    updated timestampz DEFAULT NOW()
+    created timestamptz DEFAULT NOW(),
+    updated timestamptz DEFAULT NOW()
 );
