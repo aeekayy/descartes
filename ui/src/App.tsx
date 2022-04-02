7e6e2c260525f7a376e4d7c4b3dd42c4564c8c14
Farye Nwede <farye@aeekay.com>
2022-04-02 16:38:55 -0700

    fix(ui): Adding ui for initial build

---
+++ ui/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// App Class Components
+class App extends Component {
+
+    constructor( props: {} ) {
+        super(props);
+    }
+    
+    // Render function
+    public render() {
+        return (
+            <div>Hi world</div>
+        )
+    }
+}
